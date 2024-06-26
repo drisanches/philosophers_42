21d4536116d65c344419156455071e51786ae50a
drisanches <driellysanches.s@gmail.com>
2024-06-26 00:02:32 -0300

    upt: philo actions adicionada na routine

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: dde-fati <dde-fati@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 22:58:20 by dde-fati          #+#    #+#             */
-/*   Updated: 2024/06/25 22:51:24 by dde-fati         ###   ########.fr       */
+/*   Updated: 2024/06/26 00:01:43 by dde-fati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,14 @@ void	init_data(t_data **data)
 	(*data)->num = 0;
 }
 
-void	*thread_function(void *arg)
+void	*routine(void *arg)
 {
 	t_data		*data;
 	
 	data = (t_data *)arg;
-	printf("This is thread: %d\n", data->num);
+	printf("Philosopher %d is eating\n", data->num);
+	printf("Philosopher %d is sleeping\n", data->num);
+	printf("Philosopher %d is thinking\n", data->num);
 	pthread_mutex_unlock(&data->mutex);
 	return (NULL);
 }
@@ -39,7 +41,7 @@ int	main(void)
 	while (data->num < 3)
 	{
 		pthread_mutex_lock(&data->mutex);
-		if (pthread_create(&threads[data->num], NULL, &thread_function, data) != 0)
+		if (pthread_create(&threads[data->num], NULL, &routine, data) != 0)
 			return (exit_error(1, data));
 		data->num++;
 	}
